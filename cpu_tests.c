c8c3b16037fd9cb6a2b55d7ed47258525f5c556d
smittytone <smittytone@icloud.com>
2021-08-10 09:22:25 +0100

    Add more tests

--- cpu_tests.c
+++ cpu_tests.c
@@ -10,14 +10,46 @@
 #include "main.h"
 
 
-void alu_tests() {
-    uint32_t errors = 0;
-    uint32_t passes = 0;
-    uint32_t tests = 0;
+void test(main) {
+    tests = 0;
+    errors = 0;
+    passes = 0;
 
-    // Test 1
-    tests++;
-    reg.cc = 0x00;
+    test_alu();
+    test_index();
+    test_logic();
+
+    printf("Tests: %i\n", tests);
+    printf("Passes: %i\n", passes);
+    printf("Fails: %i\n", errors);
+    printf("--------------------------------------------------\n");
+}
+
+
+void test_alu() {
+
+    // ADC
+    test_setup();
+    reg.cc = 0x0B;
+    uint8_t result = add_with_carry(0x14, 0x22);
+    if (result == 0x36 && reg.cc == 0x00) {
+        passes++;
+    } else {
+        errors++;
+    }
+
+    // ADD
+    test_setup();
+    reg.cc = 0x13;
+    uint8_t result = add_no_carry(0xF2, 0x39);
+    if (result == 0x2B && reg.cc == 0x11) {
+        passes++;
+    } else {
+        errors++;
+    }
+
+    // CMP
+    test_setup();
     compare(0xF6, 0x18);
     if (reg.cc == 0x08) {
         passes++;
@@ -25,9 +57,8 @@ void alu_tests() {
         errors++;
     }
 
-    // Test 2
-    tests++;
-    reg.cc = 0x00;
+    // COMP
+    test_setup();
     uint8_t result = complement(0x23);
     if (result == 0xDC && reg.cc == 0x09) {
         passes++;
@@ -35,9 +66,8 @@ void alu_tests() {
         errors++;
     }
 
-    // Test 3
-    tests++;
-    reg.cc = 0x00;
+    // DAA
+    test_setup();
     reg.a = add_no_carry(0x39, 0x47);
     daa();
     if (reg.a == 0x86) {
@@ -45,9 +75,88 @@ void alu_tests() {
     } else {
         errors++;
     }
+}
 
-    printf("Tests: %i\n", tests);
-    printf("Passes: %i\n", passes);
-    printf("Fails: %i\n", errors);
-    printf("--------------------------------------------------\n");
+
+void test_index() {
+
+    // ABX
+    test_setup();
+    reg.x = 0x8006;
+    reg.b = 0xCE;
+    abx();
+    if (reg.x == 0x80D4) {
+        passes++;
+    } else {
+        errors++:
+    }
+}
+
+void test_logic() {
+
+    // AND
+    test_setup();
+    reg.cc = 0x32;
+    uint8_t result = do_and(0x8B, 0x0F);
+    if (result == 0x0B && reg.cc == 0x30) {
+        passes++;
+    } else {
+        errors++:
+    }
+
+    // ANDCC
+    test_setup();
+    reg.cc = 0x79;
+    andcc(0xAF);
+    if (reg.cc == 0x29) {
+        passes++;
+    } else {
+        errors++:
+    }
+
+    // ANDCC
+    test_setup();
+    reg.cc = 0x04;
+    arith
+    if (reg.cc == 0x29) {
+        passes++;
+    } else {
+        errors++:
+    }
+
+    // ASL/LSL
+    test_setup();
+    reg.cc = 0x04;
+    uint8_t result = logic_shift_left(0xA5);
+    if (result == 0x4A && reg.cc == 0x03) {
+        passes++;
+    } else {
+        errors++:
+    }
+
+    // ASR
+    test_setup();
+    uint8_t result = arith_shift_right(0xE5);
+    if (result == 0xF2 && reg.cc == 0x09) {
+        passes++;
+    } else {
+        errors++:
+    }
+
+    // CLR
+    test_setup();
+    reg.a = 0xE2;
+    clr(CLRA, MODE_INHERENT);
+    if (reg.a == 0x00 && reg.cc == 0x04) {
+        passes++;
+    } else {
+        errors++:
+    }
+
+}
+
+
+void test_setup() {
+    tests++;
+    reg.cc = 0x00;
 }
